8b59a6325224d9e0047099ff474223ec62152b7f
renatofekete <renatofekete.91@gmail.com>
2018-05-20 21:16:14 +0200

    trending

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const babel = require('gulp-babel')
+
+gulp.task('html', function () {
+  gulp.src('src/index.html')
+    .pipe(gulp.dest('dist'));
+});
+// Javascript
+gulp.task('js', function () {
+  gulp.src('src/js/app.js')
+    .pipe(
+      babel({
+        presets: "es2015"
+      })
+    )
+    .pipe(uglify().on("error", function (e) {
+      console.log(e);
+    }))
+    .pipe(gulp.dest('dist/js'));
+})
+// Sass
+gulp.task('sass', function () {
+  gulp.src('src/scss/style.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on("error", sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('dist/css'));
+})
+
+gulp.task('default', ['html', 'js', 'sass']);
+
+gulp.task('watch', function () {
+  gulp.watch('src/*.html', ['html']);
+  gulp.watch('src/scss/*.scss', ['sass']);
+  gulp.watch('src/js/app.js', ['js']);
+});
---
+++ src/js/app.js
@@ -0,0 +1,36 @@
+const request = new XMLHttpRequest();
+const key = "JZHmTSzn6xR3pRx9WJEyFRMCF6XugnvO";
+const container = document.querySelector(".main__container");
+
+// Display GIPHS
+function loop(input) {
+  for (var i = 0; i < input.data.length; i++) {
+    container.innerHTML += `
+      <div class="main__box">
+        <div class="main__box__title">
+          <h3>${input.data[i].title}</h3>
+        </div>
+        <div class="main__box__img">
+          <img src="${input.data[i].images.fixed_height.url}"/>
+        </div>
+      </div>`;
+  }
+}
+
+function initialCall() {
+  request.onreadystatechange = function() {
+    if (request.readyState === 4 && request.status === 200) {
+      var requestData = JSON.parse(request.response);
+      console.log(requestData);
+
+      loop(requestData);
+    }
+  };
+  request.open(
+    "GET",
+    `http://api.giphy.com/v1/gifs/trending?limit=40&api_key=${key}`,
+    true
+  );
+  request.send();
+}
+initialCall();
